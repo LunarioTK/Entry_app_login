2c0b99ff3cdb2abb78e9be5a9ec5d5c8fe7931a7
Kelvin Rodrigues <81050267+LunarioTK@users.noreply.github.com>
2023-01-14 22:12:49 +0000

    Adding forms

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  TextEditingController controller = TextEditingController();
+  Login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+        child: Form(
+          child: Column(
+            children: [
+              // Email
+              TextFormField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+
+              // Password
+              TextFormField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
