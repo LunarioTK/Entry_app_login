5fad0886e26c4b25adc8f9a78436d59b1994913a
Kelvin Rodrigues <81050267+LunarioTK@users.noreply.github.com>
2023-01-16 15:18:35 +0000

    Visual

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,61 +1,99 @@
 import 'package:flutter/material.dart';
+import '../constants/uicolor.dart';
 
 class Login extends StatelessWidget {
-  TextEditingController controller = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
   Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //
-              const Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: uiColor,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
 
-              // Email
-              TextFormField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
+                const SizedBox(height: 5),
+
+                const Text(
+                  'Welcome back',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+
+                // Email
+                TextFormField(
+                  controller: emailController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              // Password
-              TextFormField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
+                // Password
+                TextFormField(
+                  controller: passController,
+                  style: const TextStyle(color: Colors.white),
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // Button for login
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Login'),
-              )
-            ],
+                // Button for login
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonsColor,
+                    elevation: 10,
+                  ),
+                  child: const Text('Login'),
+                )
+              ],
+            ),
           ),
         ),
       ),
